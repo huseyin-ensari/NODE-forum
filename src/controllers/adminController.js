7513ddd733366a5d71d8645d33bad0d503c7fc55
Hüseyin Ensari <hsynensari@gmail.com>
2021-11-30 12:14:43 +0300

    Allow admin to block the user

---
+++ src/controllers/adminController.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+const CustomError = require('../helpers/errors/CustomError');
+const asyncErrorHandler = require('express-async-handler');
+
+const blockUser = asyncErrorHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const user = await User.findById(id);
+  user.blocked = !user.blocked;
+
+  await user.save();
+
+  return res.status(200).json({
+    success: true,
+    message: 'Block - Unblock successfull',
+  });
+});
+
+module.exports = {
+  blockUser,
+};
--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -3,11 +3,15 @@ const {
   getAccessToRoute,
   getAdminAccess,
 } = require('../middlewares/authorization/authMiddleware');
+const {
+  checkUserExist,
+} = require('../middlewares/database/databaseErrorHelpers');
+const { blockUser } = require('../controllers/adminController');
 
 router.use([getAccessToRoute, getAdminAccess]);
 
 // /api/admin
-router.get('/', (req, res) => res.json({ msg: 'Admin test' }));
+router.get('/block/:id', checkUserExist, blockUser);
 
 // TODO block user
 
