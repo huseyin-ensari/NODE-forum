95dd45919dc0f9dc53a73b0a182fb8f4e71640cd
Hüseyin Ensari <hsynensari@gmail.com>
2021-12-09 00:35:20 +0300

    Add API web site

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const path = require('path');
 // project file import
 const routers = require('./src/routers');
 const customErrorHandler = require('./src/middlewares/errors/customErrorHandler');
+const pageRouter = require('./src/routers/pageRouter');
 // db connection
 require('./src/configs/dbConnection');
 // app start and express middlewares
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', path.resolve(__dirname, './src/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 // routers
 app.use('/api', routers);
+app.use('/', pageRouter);
 // middlewares
 app.use(customErrorHandler);
 //
